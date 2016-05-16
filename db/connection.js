2731c051d02f2452053ded530454c32e023a2498
Michael Nahum <USGrant63@gmail.com>
2016-05-16 15:17:58 -0400

    all the things

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,18 @@
-var seedData = require("./seeds.json");
-module.exports = {
-  candidates: seedData
-};
+var mongoose = require("mongoose");
+
+var CandidateSchema = new mongoose.Schema({
+
+  name: String,
+  year: Number
+
+});
+
+mongoose.model("Candidate", CandidateSchema);
+if(process.eng.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGOLAB_URL);
+} else {
+  mongoose.connect("mongodb://localhost/whenprez");
+}
+
+
+module.exports = mongoose;
